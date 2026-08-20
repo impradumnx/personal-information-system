ef27fec5ab92e8c47d22d911f855f0d179fc306c
Pradum Rathour <pradumrathour944@gmail.com>
2026-08-20 21:46:32 +0530

    Update database connection

--- db.php
+++ db.php
@@ -1,6 +1,18 @@
-    <?php
+<?php
 
-$conn = mysqli_connect("localhost", "root", "", "personal_info_db");
+$host = getenv("MYSQLHOST") ?: "localhost";
+$port = getenv("MYSQLPORT") ?: "3306";
+$user = getenv("MYSQLUSER") ?: "root";
+$password = getenv("MYSQLPASSWORD") ?: "";
+$database = getenv("MYSQLDATABASE") ?: "personal_info_db";
+
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
